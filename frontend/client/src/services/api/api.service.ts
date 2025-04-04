132b1448045e4f605fbb4df0f60beb02a220b4fd
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2025-04-04 20:26:25 +0300

    Crated raw API call without JSON conversion

--- frontend/client/src/services/api/api.service.ts
+++ frontend/client/src/services/api/api.service.ts
@@ -23,4 +23,23 @@ export async function callAPI<T>(
   return (await response.json()) as T;
 }
 
+export async function callAPI_raw(
+  path: string,
+  token: AuthToken,
+  method: HTTP_Methods = "GET",
+): Promise<Response> {
+  const response = await fetch(`${API_ENDPOINT}${path}`, {
+    method,
+    headers: {
+      ...token,
+    },
+  });
+
+  if (!response.ok) {
+    return Promise.reject("[API CALL !OK] -- " + response.statusText);
+  }
+
+  return response;
+}
+
 export const USE_SERVER: boolean = !(import.meta.env.MODE === "development");
